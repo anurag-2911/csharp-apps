2c159ef590cfe09cf06dbbe6b35a6b67ec912dc6
anurag <anurag.hcu@gmail.com>
2021-08-23 17:27:56 +0530

    certificate store in windows.

--- CSharpApps/CodeLibrary/TestCert.cs
+++ CSharpApps/CodeLibrary/TestCert.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +12,35 @@ namespace CodeLibrary
     {
         public void Test()
         {
+            CertificateNames();
+        }
 
+        private static void CertificateNames()
+        {
+            Console.WriteLine("\r\nExists Certs Name and Location");
+            Console.WriteLine("------ ----- -------------------------");
+
+            foreach (StoreLocation storeLocation in (StoreLocation[])
+                Enum.GetValues(typeof(StoreLocation)))
+            {
+                foreach (StoreName storeName in (StoreName[])
+                    Enum.GetValues(typeof(StoreName)))
+                {
+                    X509Store store = new X509Store(storeName, storeLocation);
+
+                    try
+                    {
+                        store.Open(OpenFlags.OpenExistingOnly);
+
+                        Console.WriteLine("Yes   {0,4}  {1}, {2}", store.Certificates.Count, store.Name, store.Location);
+                    }
+                    catch (CryptographicException)
+                    {
+                        Console.WriteLine("No {0}, {1}", store.Name, store.Location);
+                    }
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
