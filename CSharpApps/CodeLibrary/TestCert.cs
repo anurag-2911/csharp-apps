1272e872321bed460a622d55153fea46e7be38fe
anurag <anurag.hcu@gmail.com>
2021-08-23 23:58:51 +0530

    basic add certificate to store.

--- CSharpApps/CodeLibrary/TestCert.cs
+++ CSharpApps/CodeLibrary/TestCert.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -12,7 +11,12 @@ namespace CodeLibrary
     {
         public void Test()
         {
-            CertificateNames();
+            //CertificateNames();
+            string pfxFilePath = @"C:\work\data\userstory\msix\cert\mycert.pfx";
+            string certFilePath = @"C:\work\data\userstory\msix\cert\mycert.cer";
+            CreateCertificate(certFilePath,pfxFilePath);
+            AddCertToStore(certFilePath);
+            //AddCert();
         }
 
         private static void CertificateNames()
@@ -42,5 +46,82 @@ namespace CodeLibrary
                 Console.WriteLine();
             }
         }
+
+        static void CreateCertificate(string certfilepath, string pfxfilepath)
+        {
+            var ecdsa = ECDsa.Create(); // generate asymmetric key pair
+            var req = new CertificateRequest("cn=testcert", ecdsa, HashAlgorithmName.SHA256);
+            var cert = req.CreateSelfSigned(DateTimeOffset.Now, DateTimeOffset.Now.AddYears(5));
+
+            // Create PFX (PKCS #12) with private key
+            File.WriteAllBytes(pfxfilepath, cert.Export(X509ContentType.Pfx, "P@55w0rd"));
+
+            // Create Base 64 encoded CER (public key only)
+            File.WriteAllText(certfilepath,
+                "-----BEGIN CERTIFICATE-----\r\n"
+                + Convert.ToBase64String(cert.Export(X509ContentType.Cert), Base64FormattingOptions.InsertLineBreaks)
+                + "\r\n-----END CERTIFICATE-----");
+        }
+
+        private static void AddCertToStore(string certFilePath)
+        {
+            //Create new X509 store called teststore from the local certificate store.
+            X509Store store = new X509Store("teststore", StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadWrite);
+            X509Certificate2 certificate = new X509Certificate2();
+
+            //Create certificates from certificate files.
+            
+            X509Certificate2 certificate1 = new X509Certificate2(certFilePath);
+            //X509Certificate2 certificate2 = new X509Certificate2(certFilePath);
+            //X509Certificate2 certificate3 = new X509Certificate2(certFilePath);
+
+            //Create a collection and add two of the certificates.
+            //X509Certificate2Collection collection = new X509Certificate2Collection();
+            //collection.Add(certificate2);
+            //collection.Add(certificate3);
+
+            //Add certificates to the store.
+            store.Add(certificate1);
+            //store.AddRange(collection);
+
+            X509Certificate2Collection storecollection = (X509Certificate2Collection)store.Certificates;
+            Console.WriteLine("Store name: {0}", store.Name);
+            Console.WriteLine("Store location: {0}", store.Location);
+            foreach (X509Certificate2 x509 in storecollection)
+            {
+                Console.WriteLine("certificate name: {0}", x509.Subject);
+            }
+
+            //Remove a certificate.
+            store.Remove(certificate1);
+            X509Certificate2Collection storecollection2 = (X509Certificate2Collection)store.Certificates;
+            Console.WriteLine("{1}Store name: {0}", store.Name, Environment.NewLine);
+            foreach (X509Certificate2 x509 in storecollection2)
+            {
+                Console.WriteLine("certificate name: {0}", x509.Subject);
+            }
+
+            //Remove a range of certificates.
+            //store.RemoveRange(collection);
+            X509Certificate2Collection storecollection3 = (X509Certificate2Collection)store.Certificates;
+            Console.WriteLine("{1}Store name: {0}", store.Name, Environment.NewLine);
+            if (storecollection3.Count == 0)
+            {
+                Console.WriteLine("Store contains no certificates.");
+            }
+            else
+            {
+                foreach (X509Certificate2 x509 in storecollection3)
+                {
+                    Console.WriteLine("certificate name: {0}", x509.Subject);
+                }
+            }
+
+            //Close the store.
+            store.Close();
+        }
     }
+    
+    
 }
